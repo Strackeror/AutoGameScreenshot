fc49e8b24d2b404470974b0806e62b06ca1a3a65
Strackeror <rremilavergne@gmail.com>
2022-05-08 01:12:47 +0200

    Black Formatting

--- AutoGameScreenshot.py
+++ AutoGameScreenshot.py
@@ -1,16 +1,17 @@
+import asyncio
+import datetime
+import os
+import re
 from dataclasses import dataclass, field
+from pathlib import Path
+from typing import List
+
+import psutil
 import win32gui
 import win32process
-import psutil
-import os
-import datetime
 from PIL import Image, ImageGrab
-
 from pystray import Icon, Menu, MenuItem
-from typing import List
-import asyncio
-import re
-from pathlib import Path
+
 
 @dataclass
 class AgsConfig:
@@ -23,14 +24,14 @@ class AgsConfig:
     additional_exes: List[str] = field(default_factory=lambda: [])
     ignored_exes: List[str] = field(default_factory=lambda: ["steam.exe"])
 
+
 config = AgsConfig()
 
 
 def should_screenshot(hwnd) -> bool:
     (tid, pid) = win32process.GetWindowThreadProcessId(hwnd)
     process = psutil.Process(pid)
-    dlls = [os.path.basename(dll.path)
-            for dll in process.memory_maps()]
+    dlls = [os.path.basename(dll.path) for dll in process.memory_maps()]
     dlls = sorted(dlls)
 
     if process.name() in config.additional_exes:
@@ -38,6 +39,7 @@ def should_screenshot(hwnd) -> bool:
     if process.name() in config.ignored_exes:
         return False
     return any("xinput" in dll for dll in dlls)
+
 
 def screenshot(hwnd):
     x, y, x1, y1 = win32gui.GetWindowRect(hwnd)
@@ -47,15 +49,15 @@ def screenshot(hwnd):
     path = Path(config.folder).joinpath(date)
     path.mkdir(parents=True, exist_ok=True)
 
-    img_time = datetime.datetime.now().strftime('%H-%M-%S')
+    img_time = datetime.datetime.now().strftime("%H-%M-%S")
     window_name = win32gui.GetWindowText(hwnd)
     window_name = re.sub(r'[\\/*?:"<>|]', "", window_name)
 
-    
     path = path / f"{img_time}-{window_name}.jpg"
     print(f"saving screenshot to {path}")
     img.save(path, quality=config.quality)
-    
+
+
 async def background_loop():
     while True:
         hwnd = win32gui.GetForegroundWindow()
@@ -66,7 +68,10 @@ async def background_loop():
             print(f"Error: {e}")
         await asyncio.sleep(config.delay)
 
+
 background_task = None
+
+
 async def background():
     global background_task
     background_task = asyncio.create_task(background_loop())
@@ -74,18 +79,24 @@ async def background():
         await background_task
     except asyncio.CancelledError:
         print("cancelled")
-    print('Done')
+    print("Done")
+
 
 event_loop = asyncio.new_event_loop()
+
+
 async def stop():
     icon.stop()
     if background_task:
         background_task.cancel()
 
 
-icon = Icon('icon', icon=Image.open("icon.png"),
-            menu=Menu(
-                MenuItem('Exit', lambda: asyncio.run_coroutine_threadsafe(stop(), event_loop))))
+icon = Icon(
+    "icon",
+    icon=Image.open("icon.png"),
+    menu=Menu(
+        MenuItem("Exit", lambda: asyncio.run_coroutine_threadsafe(stop(), event_loop))
+    ),
+)
 icon.run_detached()
 event_loop.run_until_complete(background())
-
